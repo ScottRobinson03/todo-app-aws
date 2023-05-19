14672e626dff27c65f56e694f427875237970376
Scott Robinson <scott.robinson2@sky.uk>
2023-05-19 13:22:28 +0100

    Remove unused signUpAttribute and resort imports

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import { Authenticator } from "@aws-amplify/ui-react";
-import "@aws-amplify/ui-react/styles.css";
 import { Amplify } from "aws-amplify";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import "@aws-amplify/ui-react/styles.css";
 import awsExports from "./aws-exports";
 import "./App.css";
 
@@ -13,7 +13,7 @@ Amplify.configure(awsExports);
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <Authenticator signUpAttributes={["email", "phone_number", "name", "zoneinfo"]}>
+        <Authenticator signUpAttributes={["email", "phone_number", "name"]}>
             {({ signOut, user }) => <App signOut={signOut} user={user} />}
         </Authenticator>
     </React.StrictMode>
